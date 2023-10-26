67d424957037cf9b1c7147fd2debfdad64152fed
coolitamigo <46348062+coolitamigo@users.noreply.github.com>
2023-10-26 15:50:57 -0400

    Create main.js

---
+++ main.js
@@ -0,0 +1,33 @@
+let userName = 'Tim';
+let userQuestion = 'Will I ever get a job as a web developer?';
+let randomNumber = Math.floor(Math.random() * 8);
+let eightBall = '';
+
+if (userName === '') {
+    console.log('Hello!');
+} else {
+    console.log(`Hello, ${userName}`);
+}
+
+console.log(`${userName} asked ${userQuestion}`);
+console.log('The answer is...');
+
+if (randomNumber === 0) {
+    eightBall = 'It is certain';
+} else if (randomNumber === 1) {
+    eightBall = 'It is decidedly so';
+} else if (randomNumber === 2) {
+    eightBall = 'Reply hazy try again';
+} else if (randomNumber === 3) {
+    eightBall = 'Cannot predict now';
+} else if (randomNumber === 4) {
+    eightBall = 'Do not count on it';
+} else if (randomNumber === 5) {
+    eightBall = 'My sources say no';
+} else if (randomNumber === 6) {
+    eightBall = 'Outlook not so good'
+} else {
+    eightBall = 'Signs point to yes';
+}
+
+console.log(eightBall);
